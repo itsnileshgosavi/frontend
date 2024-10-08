971d3b39ae746d3e5b1537a71f70cec0850ad1fb
Nilesh <nbgosavi5@gmail.com>
2024-10-09 03:32:35 +0530

    implemented lazy loading

--- src/main.jsx
+++ src/main.jsx
@@ -1,43 +1,21 @@
-import { StrictMode } from 'react'
+import { StrictMode, lazy, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter } from 'react-router-dom'
 import { RouterProvider } from 'react-router-dom'
+import Loading from './components/Loading.jsx'
 import Landing from './pages/Landing.jsx'
-import SignIn from './pages/SignIn.jsx'
-import SignUp from './pages/SignUp.jsx'
-import Channel from './pages/Channel.jsx'
+const SignIn = lazy(() => import('./pages/SignIn.jsx'))
+const SignUp = lazy(() => import('./pages/SignUp.jsx'))
+const Channel = lazy(() => import('./pages/Channels.jsx'))
 import ErrorPage from './pages/ErrorPage.jsx'
 import { ThemeProvider } from './lib/theme/theme-provider.jsx'
-import VideoPage from './pages/Watch.jsx'
+const VideoPage = lazy(() => import('./pages/Watch.jsx'))
 import { Provider } from 'react-redux'
 import store from './redux/reduxStore.js'
-import SelfChannel from './pages/SelfChannel.jsx'
+const SelfChannel = lazy(() => import('./pages/SelfChannel.jsx'))
 
-
-const Videodata = [
-  {
-    "videoId": "video01",
-    "title": "Learn React in 30 Minutes",
-    "thumbnailUrl": "https://example.com/thumbnails/react30min.png",
-    "description": `A quick tutorial to get started with React.`,
-    "channelId": "channel01",
-    "uploader": "user01",
-    "views": 15200,
-    "likes": 1023,
-    "dislikes": 45,
-    "uploadDate": "2024-09-20",
-    "comments": [
-      {
-        "commentId": "comment01",
-        "userId": "user02",
-        "text": "Great video! Very helpful.",
-        "timestamp": "2024-09-21T08:30:00Z"
-      }
-    ]
-  }
-];
 
 
 const router = createBrowserRouter([
@@ -51,27 +29,27 @@ const router = createBrowserRouter([
       },
       {
         path: '/channel',
-        element: <SelfChannel  />,
+        element: <Suspense fallback={<Loading />}><SelfChannel /></Suspense>,
       },
       {
         path: '/channel/:handle',
-        element: <Channel  />,
+        element: <Suspense fallback={<Loading />}><Channel /></Suspense>,
       },
       {
         path: "/watch/:videoId",
-        element: <VideoPage videoData={Videodata[0]} />,
+        element: <Suspense fallback={<Loading />}><VideoPage /></Suspense>,
       }
     ],
     errorElement: <ErrorPage />,
   },
   {
     path: '/sign-in',
-    element: <SignIn />,
+    element: <Suspense fallback={<Loading />}><SignIn /></Suspense>,
     errorElement: <ErrorPage />,
   },
   {
     path: '/sign-up',
-    element: <SignUp />,
+    element: <Suspense fallback={<Loading />}><SignUp /></Suspense>,
     errorElement: <ErrorPage />,
   },
 
